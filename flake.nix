25ea5319af69591a50175380a4da6f177fdffd17
Miikka 'otter' Saukko <msaukko@users.noreply.github.com>
2024-11-18 13:36:28 +0200

    Remove secpkgs

--- flake.nix
+++ flake.nix
@@ -4,16 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    secpkgs.url = "github:juliosueiras-nix/nix-security";
-    secpkgs.inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, secpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, ... }@inputs: 
   {
     nixosConfigurations = {
       default = nixpkgs.lib.nixosSystem {
